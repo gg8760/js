0fc211efd6e7e6fb1606f58a977a47cdad20d75e
lifeng <73932388+lifeng-Lee@users.noreply.github.com>
2021-10-28 20:43:28 +0800

    Add files via upload

---
+++ jiuzhang/jiuzhang_account_parameter.js
@@ -0,0 +1,66 @@
+// 九章头条
+
+var jiuzhang = [
+    {
+        "accountInfo": "九章1-峰",
+        "accountMobile": "18253169060",
+        "withdrawAccount": "vx峰",
+        "token": "UM4GDuSJj6r8YvrjNhi5-F07kiD_GUop",
+    },
+
+    {
+        "accountInfo": "九章2-峰2",
+        "accountMobile": "",
+        "withdrawAccount": "vxArduino",
+        "token": "55nLV5tA48s-Qjr5uvlSw3o9p1W-BW0_",
+    },
+    
+    {
+        "accountInfo": "九章3-峰0410",
+        "accountMobile": "",
+        "withdrawAccount": "vx峰0410",
+        "token": 'FiPRNaeQqJFGTcqeLrxK_piyZ74KA3a8',
+    },
+
+    
+    {
+        "accountInfo": "九章4-格",
+        "accountMobile": "",
+        "withdrawAccount": 'vx格',
+        "token": '98bKpGyobzrMiUF49I06T6lz4-uu7flt',
+    },
+
+    {
+        "accountInfo": "九章5-峰0306",
+        "accountMobile": "",
+        "withdrawAccount": 'vx峰0306',
+        "token": '38RH-bvbFxsLHRq7GefCeP0-87i2yNK5',
+    },
+
+    {
+        "accountInfo": "九章6-格0306",
+        "accountMobile": "",
+        "withdrawAccount": 'vx格0306',
+        "token": 'GHo8Cr5U0TLg8C9VItTfAac0hZ3zykEU',
+    },
+
+    {
+        "accountInfo": "九章7-vx怀念",
+        "accountMobile": "",
+        "withdrawAccount": 'vx怀念',
+        "token": 'hxg59dJ5Kvv2G507rW9iJk6KRYs8eECB',
+    },
+    
+    {
+        "accountInfo": "九章8-vxba",
+        "accountMobile": "",
+        "withdrawAccount": 'vxba',
+        "token": '463wOcAQdgxqm7UoeCVmslMsKflnHNaN',
+    },
+    
+    
+    
+];
+
+
+module.exports = jiuzhang;
