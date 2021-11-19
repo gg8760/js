e3cb18754ffe99420d4a8d08dd2841cd2cac2e64
lifeng <73932388+lifeng-Lee@users.noreply.github.com>
2021-11-19 13:23:29 +0800

    Update jiuzhang_account_withdraw.js

--- jiuzhang/jiuzhang_account_withdraw.js
+++ jiuzhang/jiuzhang_account_withdraw.js
@@ -7,7 +7,7 @@ cron "20 8 * * *"  script-path=jiuzhang_account_withdraw.js,tag=九章头条-提
 
 const $ = new Env('九章头条-提现');//声明必须
 
-let jiuzhang = require('./raw_main_九章头条_account_parameter');
+let jiuzhang = require('./raw_main_jiuzhang_account_parameter');
 let cookie = "";
 let ad_readTime = 25 //看广告用时
 let withdrawMoney = '0.3'
