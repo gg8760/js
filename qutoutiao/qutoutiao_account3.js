12456ccac9205e22b3b84450accfb7a2b3e1768f
lifeng <73932388+lifeng-Lee@users.noreply.github.com>
2021-11-20 20:19:04 +0800

    Update qutoutiao_account3.js

--- qutoutiao/qutoutiao_account3.js
+++ qutoutiao/qutoutiao_account3.js
@@ -88,9 +88,9 @@ const signinfo = {
     await sleepStatus()
     await sleepReward()
     await sleep()
-    if (signinfo.sleepStatus.data.fortune_bag_can_reward) {
+//     if (signinfo.sleepStatus.data.fortune_bag_can_reward) {
       await sleepBag()
-    }
+//     }
   }
   if (new Date().getHours() >= 20) {
     await sleepinit()
