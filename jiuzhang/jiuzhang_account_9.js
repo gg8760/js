07d1d6d80367a046682d1133a2711059f3c375e7
lifeng <73932388+lifeng-Lee@users.noreply.github.com>
2021-12-31 18:31:28 +0800

    Update jiuzhang_account_9.js

--- jiuzhang/jiuzhang_account_9.js
+++ jiuzhang/jiuzhang_account_9.js
@@ -12,7 +12,7 @@ const $ = new Env('九章头条9-vx星云');//声明必须
 let jiuzhang = require('./raw_main_jiuzhang_account_parameter');
 let cookie = "";
 let readTime = 30
-let ad_readTime = 35 //看广告用时
+let ad_readTime = 30 //看广告用时
 
 !(async () => {
 
