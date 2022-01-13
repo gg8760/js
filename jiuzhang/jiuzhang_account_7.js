32bcd3b91c3ca07e48e83d9e8bdc1af2e9b75960
lifeng <73932388+lifeng-Lee@users.noreply.github.com>
2022-01-13 10:31:44 +0800

    Update jiuzhang_account_7.js

--- jiuzhang/jiuzhang_account_7.js
+++ jiuzhang/jiuzhang_account_7.js
@@ -13,7 +13,7 @@ let header = require('./raw_main_jiuzhang_account_parameter')["header"];
 
 let cookie = "";
 let readTime = 30
-let ad_readTime = 20 + Math.floor(Math.random() * 10) //看广告用时
+let ad_readTime = 18 //看广告用时
 
 let breakvarticle = false
 !(async () => {
@@ -27,7 +27,7 @@ let breakvarticle = false
     console.log(`\n ======脚本执行 ${bjTime}======\n`);
 
     let itemDic = jiuzhang[6];
-    let accountInfo = itemDic["accountInfo"]
+    accountInfo = itemDic["accountInfo"]
     cookie = itemDic["token"]
     header.token = cookie
 
@@ -186,9 +186,9 @@ function readFinish(id, type, currenIndex) {
             try {
                 let obj = JSON.parse(data)
                 console.log(`本次阅读${ctype}获得金币: ${obj.data.coin}`)
-                if (obj.data.coin < 5) {
-                    breakvarticle = true
-                }
+//                 if (obj.data.coin < 5) {
+//                     breakvarticle = true
+//                 }
 
                 console.log(`\n等待2秒，开始下一篇`)
                 await $.wait(2 * 1000);
