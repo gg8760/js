ebaf2ce22b8960cf0a8c40ce0cd465eec6d7f792
lifeng <73932388+lifeng-Lee@users.noreply.github.com>
2022-01-14 10:04:26 +0800

    Update jiuzhang_account_3.js

--- jiuzhang/jiuzhang_account_3.js
+++ jiuzhang/jiuzhang_account_3.js
@@ -80,7 +80,6 @@ function checkReceive() {
   })
 }
 
-// https://api.st615.com/v2/task/receive
 function getReceiveReward() {
   console.log(`\n🍒 账号${accountInfo} 领取限时福利🍒\n`)
   return new Promise((resolve, reject) => {
@@ -88,6 +87,11 @@ function getReceiveReward() {
       try {
         let obj = JSON.parse(data)
         console.log(obj)
+
+        let adTime = ad_readTime + Math.floor(Math.random() * 10)
+        console.log(`观看视频广告${adTime}秒,领取翻倍`)
+        await $.wait(adTime * 1000);
+        await getVideoBenefit(98)
 
       } catch (e) {
 
@@ -97,6 +101,7 @@ function getReceiveReward() {
     })
   })
 }
+
 
 function shareWechat() {
     return new Promise((resolve, reject) => {
